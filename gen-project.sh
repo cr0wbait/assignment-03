c270964c666363bf909ca8412bdd4e4a20551354
Evan Cothran <EvanCothran@Macintosh-00254bcccd10.local>
2017-01-17 14:40:56 -0500

    initial commit

---
+++ gen-project.sh
@@ -0,0 +1,4 @@
+mkdir ~/TIY/assignments/$1
+mkdir ~/TIY/assignments/$1/css ~/TIY/assignments/$1/js ~/TIY/assignments/$1/images
+touch ~/TIY/assignments/$1/css/style.css ~/TIY/assignments/$1/js/app.js
+touch ~/TIY/assignments/$1/README.md ~/TIY/assignments/$1/index.html
